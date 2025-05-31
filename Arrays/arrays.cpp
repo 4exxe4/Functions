8f28cfb641be0b41438d5a3eaa29ec1003ca0bba
Stell <Stell@MATTHEWCHEX>
2025-05-31 04:05:31 +0300

    Functions for 2D arrays

--- Arrays/arrays.cpp
+++ Arrays/arrays.cpp
@@ -3,11 +3,16 @@ using namespace std;
 
 #define tab "\t";
 
+const int ROWS = 3;
+const int COLS = 4;
+
 void FillRand(int arr[], const int n, int minRand = 0, int maxRand = 100);
 void FillRand(double arr[], const int n, int minRand = 0, int maxRand = 100);
+void FillRand(int arr[ROWS][COLS], const int ROWS, const int COLS);
 
 void Print(int arr[], const int n);
 void Print(double arr[], const int n);
+void Print (int arr[ROWS][COLS], const int ROWS, const int COLS);
 
 void Sort(int arr[], const int n);
 void Sort(double arr[], const int n);
@@ -48,6 +53,12 @@ void main()
 	//Print(brr, SIZE);
 	Sort(brr, SIZE);
 	//Print(brr, SIZE);
+	
+	
+	int i_arr_2[ROWS][COLS];
+	FillRand(i_arr_2, ROWS, COLS);
+	Print(i_arr_2, ROWS, COLS);
+
 }
 
 void FillRand(int arr[], const int n, int minRand, int maxRand)
@@ -67,6 +78,16 @@ void FillRand(double arr[], const int n, int minRand, int maxRand)
 		arr[i] /= 100;
 	}
 }
+void FillRand(int arr[ROWS][COLS], const int ROWS, const int COLS)
+{
+	for (int i = 0; i < ROWS; i++)
+	{
+		for (int j = 0; j < COLS; j++)
+		{
+			arr[i][j] = rand() % 100;
+		}
+	}
+}
 
 void Print(int arr[], const int n)
 {
@@ -84,6 +105,18 @@ void Print(double arr[], const int n)
 	}
 	cout << endl;
 
+}
+void Print(int arr[ROWS][COLS], const int ROWS, const int COLS)
+{
+	for (int i = 0; i < ROWS; i++)
+	{
+		for (int j = 0; j < COLS; j++)
+		{
+			cout << arr[i][j] << tab;
+		}
+		cout << endl;
+	}
+	cout << endl;
 }
 
 void Sort(int arr[], const int n)
